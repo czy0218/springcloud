ea89932c4a2c9c3c8d0ce6a44f1a5bcaaf1e7f06
czy0218 <chenzy@cloud-young>
2019-01-03 17:52:16 +0800

    add zipkin

---
+++ hello-spring-cloud-zipkin/src/main/java/com/czy/hello/spring/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,18 @@
+package com.czy.hello.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * Created by czy0218 on 2019/1/3.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
